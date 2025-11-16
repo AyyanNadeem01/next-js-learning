9351ab688c34f324f930a55838b08f297d246a8c
AyyanNadeem01 <ayyan137a@gmail.com>
2025-11-16 17:52:46 +0500

    prepared data for fake api for static page generation at build time

--- app/(applications)/blogs/[blog]/page.js
+++ app/(applications)/blogs/[blog]/page.js
@@ -1,22 +1,25 @@
 import { notFound } from 'next/navigation';
 import React from 'react';
-export function generateStaticParams() {//these will be generated at build time
-  return [
-    {
-      blog: "1",
-    },
-    {
-      blog: "2",
-        },    
-        {
-      blog: "3",
-    }, {
-      blog: "4",
-        },    
-        {
-      blog: "5",
-    }
-  ];
+export async function generateStaticParams() {//these will be generated at build time
+  const respnse=await fetch("https://jsonplaceholder.typicode.com/todos")
+  const data=await respnse.json()//now the data for fake api is statically generated at build time and will not be fetched at runtime and also the dynamic routes will be pre generated at build time and for above 200 id then the blog will be rendered at runtime
+  console.log(data)
+  return data.map((id)=>({blog:`${id}`}))
+  // return [
+  //   {    blog: "1",
+  //   },
+  //   {
+  //     blog: "2",
+  //       },    
+  //       {
+  //     blog: "3",
+  //   }, {
+  //     blog: "4",
+  //       },    
+  //       {
+  //     blog: "5",
+  //   }
+  // ];
 }
 const page = async({ params }) => {
   const { blog } = await params; // no need for 'await' here, params is synchronous
