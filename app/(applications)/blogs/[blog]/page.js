37f086f206d62e03e094ce4d9f588b7c6388ad22
AyyanNadeem01 <ayyan137a@gmail.com>
2025-11-18 01:02:54 +0500

    dynamic rendering , static rendering, error, auto

--- app/(applications)/blogs/[blog]/page.js
+++ app/(applications)/blogs/[blog]/page.js
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation';
 import React from 'react';
+
 // 1. What is Incremental Static Regeneration (ISR)?
 // ISR is a Next.js feature that allows statically generated pages (SSG) to be updated after build time without rebuilding the entire site.
 // You pre-render pages at build time (SSG)
@@ -12,23 +13,24 @@ import React from 'react';
 
 //these things will be tested only in production build not in dev mode
 export const dynamicParams = false;//by default it is true //this will not allow any new dynamic route other than the one which is pre generated at build time
-export const revalidate=false;//this will disable the isr and the page will be statically generated at build time only
+export const revalidate = 0;//this will disable the isr and the page will be statically generated at build time only
 //if we set it to true then the page will be regenerated at every request
 //export const revalidate=10//if we set it to some number like 10 , then the page will be regenerated after every 10 seconds but only when there is a request for that page
 
+//SSG
 export async function generateStaticParams() {//these will be generated at build time
-  const respnse=await fetch("https://jsonplaceholder.typicode.com/todos")
-  // const respnse=await fetch("https://jsonplaceholder.typicode.com/todos",
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+  // const response=await fetch("https://jsonplaceholder.typicode.com/todos",
   //   {next:{revalidate:100}})//this fetch will be revalidated after 100 seconds, it is same as defining revalidate above but this is only for this fetch request
 
-  const data=await respnse.json()//now the data for fake api is statically generated at build time and 
+  const data = await response.json()//now the data for fake api is statically generated at build time and 
   // will not be fetched at runtime and also the dynamic routes will be pre generated at build time and
   //  for above 200 id then the blog will be rendered at runtime
   //for every id which is not pre generated the first request will take time as it will be generated at runtime
   //and one generated , then next time it will be served from cache and use already generated code from server
   console.log(data)
 //one more thing to note if the page has some dynamic data to display then the variable revalidate will work otherwise it will not work
-  return data.map((id)=>({blog:`${id}`}))
+  return data.map((item) => ({ blog: `${item.id}` }))
   // return [
   //   {    blog: "1",
   //   },
@@ -45,10 +47,10 @@ export async function generateStaticParams() {//these will be generated at build
   //   }
   // ];
 }
-const page = async({ params }) => {
-  const { blog } = await params; // no need for 'await' here, params is synchronous
 
-  // Check if blog is a number
+const page = async({ params }) => {
+  const { blog } = await params; 
+
   const checkIsNumber = () => !isNaN(blog);
 
   if (!checkIsNumber()) {
--- app/(marketing)/services/page.js
+++ app/(marketing)/services/page.js
@@ -1,8 +1,20 @@
+import { cookies } from "next/headers"
 import Link from "next/link"
+//export const dynamic="force-dynamic"//this will make this page dynamic rendering always
+//export const dynamic="force-static"//this will make this page static rendering always , even if we use searchParams or cookies etc
+//export const dynamic="error"//this will throw error if we try to make this page dynamic by using searchParams or cookies etc
+
 export const metadata={
   title:"Services",
 }
-const page = () => {
+const page = async({searchParams}) => {
+  const search=await searchParams//this also makes the page dynamic
+  console.log(search)//if we comment these two lines then it will be static again even if we destructure searchParams
+  
+  // const myCookies=await cookies()
+  // console.log(myCookies)//this also makes the page dynamic
+  //there are other way that can allow dynamic rendering like headers etc
+  console.log("Running Services Page")
   return (
     <div>
       All services
