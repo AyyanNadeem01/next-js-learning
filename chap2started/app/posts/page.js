d1e2fcbdd1b32cfe5985e1360f94df0e86769893
AyyanNadeem01 <ayyan137a@gmail.com>
2025-11-18 16:34:31 +0500

    some folder maintainene, and data fetching client components

---
+++ chap2started/app/posts/page.js
@@ -0,0 +1,35 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+const Page = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
+      const data = await response.json();
+      setPosts(data);
+    }
+    fetchPosts();
+  }, []);
+
+  return (
+    <>
+    <h1>Posts</h1>
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-6">
+  {posts.map(({ id, title, body }) => (
+    <div
+      key={id}
+      className="bg-blue-400 text-white p-5 rounded-xl shadow hover:shadow-xl transition"
+    >
+      <h2 className="text-lg font-semibold mb-2">{title}</h2>
+      <p className="text-gray-600">{body}</p>
+    </div>
+  ))}
+</div>
+
+    </>
+);
+};
+
+export default Page;
