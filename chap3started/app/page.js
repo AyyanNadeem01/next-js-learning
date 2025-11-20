83ebed1f60bffb7fd3895442f0a2907022aa1309
AyyanNadeem01 <ayyan137a@gmail.com>
2025-11-20 19:05:50 +0500

    understood how to use sass in nextjs

--- chap3started/app/page.js
+++ chap3started/app/page.js
@@ -1,4 +1,5 @@
 import styles from"./home.module.css"
+import style2 from "./home.module.scss"
 //import "./globals.css"
 export const dynamic="force-dynamic";//this will make the page dynamic and will run on every request
 const Home = () => {
@@ -8,7 +9,7 @@ const Home = () => {
 
       <div>
         <h1 className={styles.title}>Home Page</h1>
-        <p>Welcome to our website!</p>
+        <p className={style2.para}>Welcome to our website!</p>
       </div>
     </>
   );
