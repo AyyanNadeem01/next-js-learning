2d4f9543a34594ae59e54963ee0b51599d45c585
AyyanNadeem01 <ayyan137a@gmail.com>
2025-11-22 14:48:53 +0500

    database setup and implemmented crud with mongodb

--- backend-start/app/todos/[id]/route.js
+++ backend-start/app/todos/[id]/route.js
@@ -1,6 +1,7 @@
+import Todo from "@/models/todoModel";
 import todosData from "../../../todos";
 import {writeFile} from "node:fs/promises"
-
+import {connectDB} from "@/lib/connectDB";
 export async function GET(_,{params}) {
   const {id}=await params
   const todo=await todosData.find((todo)=>todo.id===parseInt(id))
@@ -12,42 +13,71 @@ export async function GET(_,{params}) {
 
 }
 export async function PUT(request, { params }) {
+  // const editTodoData = await request.json();
+  // const { id } = await params;
+
+  // const index = todosData.findIndex(todo => String(todo.id) === String(id));
+  // if(editTodoData.id){
+  //   return Response.json(
+  //     { error: "Cannot edit todo id" },
+  //     { status: 403 }
+  //   )
+  //   }
+  // if (index === -1) {
+  //   return Response.json(
+  //     { error: `Todo with id ${id} not found` },
+  //     { status: 404 }
+  //   );
+  // }
+
+  // const updatedTodo = {
+  //   ...todosData[index],
+  //   ...editTodoData
+  // };
+
+  // todosData[index] = updatedTodo;
+
+  // await writeFile("todos.json", JSON.stringify(todosData, null, 2));
+
+  // return Response.json(updatedTodo);
   const editTodoData = await request.json();
+  const {id}=await params
+  const editedTodo=await Todo.findByIdAndUpdate(id,editTodoData,{
+    new:true,
+  //  runValidators:true
+  })
+  return Response.json(editedTodo)
+  
+}
+
+export async function DELETE(_,{params}){
+//   const {id}=await params
+//   const index=todosData.findIndex((todo)=>String(todo.id)===String(id))
+//   if(index===-1){
+//     return Response.json({error:`Todo with id ${id} not found`},{status:404})
+//   }
+//   const deletedTodo=todosData.splice(index,1)[0]
+//   await writeFile("todos.json",JSON.stringify(todosData,null,2))
+//  return Response.json(deletedTodo, { status: 200 })
+  await connectDB();
+
   const { id } = await params;
 
-  const index = todosData.findIndex(todo => String(todo.id) === String(id));
-  if(editTodoData.id){
-    return Response.json(
-      { error: "Cannot edit todo id" },
-      { status: 403 }
-    )
-    }
-  if (index === -1) {
+  const deletedTodo = await Todo.findByIdAndDelete(id);
+
+  if (!deletedTodo) {
     return Response.json(
       { error: `Todo with id ${id} not found` },
       { status: 404 }
     );
   }
 
-  const updatedTodo = {
-    ...todosData[index],
-    ...editTodoData
-  };
-
-  todosData[index] = updatedTodo;
-
-  await writeFile("todos.json", JSON.stringify(todosData, null, 2));
-
-  return Response.json(updatedTodo);
-}
-
-export async function DELETE(_,{params}){
-  const {id}=await params
-  const index=todosData.findIndex((todo)=>String(todo.id)===String(id))
-  if(index===-1){
-    return Response.json({error:`Todo with id ${id} not found`},{status:404})
-  }
-  const deletedTodo=todosData.splice(index,1)[0]
-  await writeFile("todos.json",JSON.stringify(todosData,null,2))
- return Response.json(deletedTodo, { status: 200 })
+  return Response.json(
+    {
+      id: deletedTodo._id.toString(),
+      text: deletedTodo.text,
+      completed: deletedTodo.completed,
+    },
+    { status: 200 }
+  );
 }
--- backend-start/app/todos/route.js
+++ backend-start/app/todos/route.js
@@ -13,31 +13,46 @@ import { readFile } from "node:fs/promises";
 import todosData from "../../todos";
 import {writeFile} from "node:fs/promises"
 import {connectDB} from "@/lib/connectDB";
+import Todo from "@/models/todoModel";
+export async function GET() {
+  await connectDB();
 
-export async function GET(request) {
-  await connectDB()
-  console.log(request)
-   const todoJSONString=await readFile("todos.json","utf-8")
-   const todos=JSON.parse(todoJSONString)
-   return Response.json(todos);
+  // const newTodo = await Todo.create({
+  //   text: "Learn JavaScript"
+  // });
+  // console.log(newTodo);
 
-   //   return new Response(JSON.stringify(todosData), {
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     status: 200,
-  //     statusText: "ProCodrr",
-  //   });
+  // const todoJSONString = await readFile("todos.json", "utf-8");
+  // const todos = JSON.parse(todoJSONString);
+  // console.log("File todos:", todos);
+
+  const todos = await Todo.find().lean();
+
+  const formatted = todos.map(t => ({
+    id: t._id.toString(),
+    text: t.text,
+    completed: t.completed
+  }));
+
+  return Response.json(formatted);
 }
+
 export async function POST(request) {
+  // const todo=await request.json()
+  // console.log(todo);
+  // const newTodo={
+  //   id:crypto.randomUUID(),
+  //   text:todo.text,
+  //   completed:false,
+  // }
+  // todosData.push(newTodo)
+  // await writeFile("todos.json",JSON.stringify(todosData,null,2))
+  // return Response.json(newTodo,{status:201})
   const todo=await request.json()
-  console.log(todo);
-  const newTodo={
-    id:crypto.randomUUID(),
-    text:todo.text,
-    completed:false,
-  }
-  todosData.push(newTodo)
-  await writeFile("todos.json",JSON.stringify(todosData,null,2))
-  return Response.json(newTodo,{status:201})
+  const newTodo=await Todo.create({
+    text:todo.text
+  })
+  return Response.json(newTodo,{
+    status:201,
+  })
 }
