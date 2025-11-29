3b80e59ac195cf7e5689bc1e385b6ea324944296
AyyanNadeem01 <ayyan137a@gmail.com>
2025-11-30 01:07:50 +0500

    rewrite() function and nextResponse

--- backend-start/middleware.js
+++ backend-start/middleware.js
@@ -15,3 +15,15 @@ export const middleware = (request) => {
 export const config = {
   matcher: ["/", "/login", "/register"],
 };
+
+//next js provide us with next response object to manipulate the response and rewrite the url meant we can change the url
+//example: if user try to access /home we can rewrite the url to /
+// import { NextResponse } from "next/server";
+
+// export const middleware = (request) => {
+//   return NextResponse.rewrite(new URL("/", request.nextUrl.origin));
+// };
+
+// export const config = {
+//   matcher: ["/home"],
+// };
