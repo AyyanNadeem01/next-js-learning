2020fff58567e5a86a525da05202bb43f5eb7a80
AyyanNadeem01 <ayyan137a@gmail.com>
2025-11-30 02:01:12 +0500

    i18n basic setup might be depriciated after sometime

---
+++ i188n/next-intl.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next-intl').NextIntlConfig} */
+const config = {
+  locales: ["en", "hi"],
+  defaultLocale: "en",
+  messagesDirectory: "./messages"
+};
+
+export default config;
