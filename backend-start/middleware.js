ceac6f44884dbf024bdb9a6727fe366e71f8559d
AyyanNadeem01 <ayyan137a@gmail.com>
2025-11-30 01:17:36 +0500

    edge runtime and making a page/component edge runtime enable

--- backend-start/middleware.js
+++ backend-start/middleware.js
@@ -27,3 +27,11 @@ export const config = {
 // export const config = {
 //   matcher: ["/home"],
 // };
+
+
+//for next js middleware is always avaible in edge runtime so we cant use node js modules like fs or path or any other module that rely on node js apis
+//so if we import "fs" in middleware file it will throw error like "fs module is not found" because fs module is a node js module and node js modules are not avaible in edge runtime
+//but in normal files like page.js or api route we can use node js modules because those files are not executed in edge runtime
+
+//lets say we had a page /about, a  server component , and we want that it should be in edge runtime then we can make it using below code
+//export const runtime = "edge";//this will make the about page to be executed in edge runtime. it will not support node js modules
